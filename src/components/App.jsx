d6d94c7ea47944abcadb6d8b44eadd88694b2c6e
Macm91 <macm91.mc@gmail.com>
2021-09-15 22:32:00 -0400

    Deleted unnecessary files. Created Components folder. Created the App.jsx folder, and in it I input functions to use to call up functions in our django backend

---
+++ src/components/App.jsx
@@ -0,0 +1,71 @@
+import React, {Component} from "react";
+import './App.css'
+import axios from "axios";
+
+
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state ={
+            comments : [],
+            search :'',
+            searchResults : ''
+            
+        }
+    }
+    componentDidMount(){
+        // To DO: input the get all videos function here. 
+        // This can be filtered in the display via the search bar. 
+        
+    }
+
+// Done
+    async getComments (){
+        try{
+            let response = await axios.get('http://127.0.0.1:8000/comment/');
+            this.setState({
+                comments: response.data
+            });
+    }
+        catch(ex) {
+            console.log ('Error in API Call!')
+        }
+    }
+
+// Done
+    createComment=(newComment)=>{axios.post('http://127.0.0.1:8000/comment/', newComment)}
+    
+    deleteComment=(comment)=>{
+        axios.delete('http://127.0.0.1:8000/music/'+comment.id+'/')
+        this.getSongs()
+    }
+
+   
+// Done
+    handleChange= (event) => {
+         this.setState ({
+        [event.target.name]: event.target.value,
+    });
+    }   
+
+    editComment = (comment) => {
+        axios.put('http://127.0.0.1:8000/comment/'+comment.id+'/')
+    }
+
+
+// ###############
+// TODO: Need to create/implement from the Youtube API a filter function that will fitler by video 
+
+
+
+
+// Need to fill this in, obviously. 
+    render (){
+        return(
+            <div className="container-fluid">
+            </div>
+        )
+    }
+}
+
+export default App
