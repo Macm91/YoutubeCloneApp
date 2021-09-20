5a35070f54f8a4cab0ca51529b7d17ed3c519fd5
unknown <dtulpa16@yahoo.com>
2021-09-20 10:40:10 -0500

    related videos will now render in embedded player when clicked

--- src/components/DisplayComments/DisplayComments.jsx
+++ src/components/DisplayComments/DisplayComments.jsx
@@ -2,28 +2,30 @@ import React, { useState } from "react";
 
 
     // filters to display only comments with the exact videoID    
-const DisplayComments =(props)=> {
-    const [comments, setComments] = useState([props.comments]);
-    const [video, setVideo]= useState ("");
+// const DisplayComments =(props)=> {
+//     const [comments, setComments] = useState([props.comments]);
+//     const [video, setVideo]= useState ("");
     
-    setVideo(props.video);
-    // props.filterComments(video);
+//     setVideo(props.video);
+//     props.filterComments(video);
       
 
-    // async filterComments(video){
-    //     try{
-    //         let response = await axios.get(`http://127.0.0.1:8000/comment/${video}/video`);
-    //         this.setState({
-    //             comments: response.data
-    //         });
+//     async function filterComments(video){
+//         try{
+//             let response = await axios.get(`http://127.0.0.1:8000/comment/${video}/video`);
+//             this.setState({
+//                 comments: response.data
+//         });
     
-    return(
-        <div>
-            {console.log("Comments State: ",comments)}
-            {/* {comments.map((comment, index) => 
-                <p key= "index" className= "individualComment">{comment.comment}</p>)} */}
-        </div>
-    )
-}
+//     return(
+//         <div>
+//             {console.log("Comments State: ",comments)}
+//             {comments.map((comment, index) => 
+//                 <p key= "index" className= "individualComment">{comment.comment}</p>)}
+//         </div>
+//       )  
+//     }
+//     }
+    
 
-export default DisplayComments
+// export default DisplayComments
--- src/components/VideoViewer/VideoViewer.jsx
+++ src/components/VideoViewer/VideoViewer.jsx
@@ -30,21 +30,32 @@ class VideoViewer extends Component {
     } 
 
 
-    async filterComments(video){
-    try{
-        let response = await axios.get(`http://127.0.0.1:8000/comment/${video}/video`);
-        this.setState({
-            comments: response.data
-        });
-}
-    catch(ex) {
-        console.log ('Error in API Call!');
-    }
-} 
+//     async filterComments(video){
+//         try{
+//             let response = await axios.get(`http://127.0.0.1:8000/comment/${video}/video`);
+//             this.setState({
+//                 comments: response.data
+//             });
+//     }
+//         catch(ex) {
+//             console.log ('Error in API Call!');
+//         }
+// } 
 
-    clickMe = (e) => {
-        console.log(e)
-        console.log('ya done clicked on me')
+    // async getTitleAndDescription(videoToPlay){
+    //     let response
+    //     response = await axios.get(`https://www.googleapis.com/youtube/v3/videos?id=${this.state.video}&key=AIzaSyBkY3kg-L1IHpD3Wy285XtpITZBj5oMONQ
+    //     &part=snippet,statistics`).then(response =>  { response = (response.data.items)})
+    //     return(
+    //         <p>{response.snippet.title}</p>
+    //     )
+    // }
+
+
+    loadNewVid = (vid) =>{
+        this.setState({
+            video: vid
+        })
     }
     
 
@@ -59,9 +70,9 @@ class VideoViewer extends Component {
                 src={`https://www.youtube.com/embed/${this.state.video}`}
                 frameborder="0"></iframe>
                 <CreateComment createComment = {this.createComment}/>
-                <DisplayComments comments= {this.comments} video={this.video} filterComments={this.filterComments}/>        
+                {/* <DisplayComments comments= {this.state,comments} video={this.state.video} filterComments={this.filterComments}/>         */}
             </div>
-            <RelatedVideos video = {this.state.video} />
+            <RelatedVideos video = {this.state.video} newLoad = {this.loadNewVid}/>
             </React.Fragment>
          );
     }
