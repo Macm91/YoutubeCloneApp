504ed6a0cd1ad0834d9b2dd3a56e9b63e53c8ab7
unknown <dtulpa16@yahoo.com>
2021-09-21 18:49:07 -0500

    completed reply function

--- src/components/DisplayComments/DisplayComments.jsx
+++ src/components/DisplayComments/DisplayComments.jsx
@@ -1,5 +1,6 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, Component} from "react";
 import axios from "axios";
+import DisplayReplies from "../DisplayReplies/DisplayReplies";
 
 
 
@@ -8,35 +9,62 @@ import axios from "axios";
 const DisplayComments=(props)=>{
     const [comments, setComments] = useState(['no Comments'])
     const [comm, setComm] = useState([])
+    const [reply,setReply] = useState([''])
+    const [replies,setReplies] = useState([''])
 
-    function filterComment(video){
-        axios.get(`http://127.0.0.1:8000/comment/${video}/video/`).then(response=>{setComments(response.data)})
+    
+
+    async function filterComment(video){
+        await axios.get(`http://127.0.0.1:8000/comment/${props.video}/video/`).then(response=>{setComments(response.data)}) 
+    }
+    // const startFilteredComments = () =>{
+    //     filterComment().then(filterReplies)
+    // }
+    async function updateComment(comment){ 
+        await axios.put(`http://127.0.0.1:8000/comment/${comment}/`)
     }
 
-    const updateComment = (comment) => { 
-        axios.put('http://127.0.0.1:8000/comment/'+comment.id+'/')
-    }
+    // async function filterReplies(val){
+    //     console.log(val)
+    //     await axios.get(`http://127.0.0.1:8000/comment/${val}/reply`).then(response => setReplies(response.data.reply))
+    // }    
 
     const increment = (val)=>{
-        debugger
-        setComm(
+
+        let commentUpdate = 
             {
-                    id: val.id,
-                    video: val.video,
-                    comment: val.comment,
-                    likes: val.like,
-                    dislikes: val.dislikes
+                    "id":val.id,
+                    "video":val.video,
+                    "comment":val.comment,
+                    "likes":(val.like=+1),
+                    "dislikes":val.dislikes
             }
-        )
+        
         console.log("setComm")
         console.log (comm)
-        updateComment(comm);
+        updateComment(commentUpdate);
+    }
+    
+    const handleSubmit = (e,id) =>{
+        e.preventDefault()
+        getReplies(id)
     }
 
+    async function getReplies(id){
+        let newReply = {comment_id: id, reply: reply}
+        await axios.post(`http://127.0.0.1:8000/reply/`,newReply)
+    }
 
-useEffect (()=>{
-    filterComment(props.video);
-}, [props])
+    const handleChange= (event) => {
+        setReply (event.target.value);
+    }
+
+    useEffect (()=>{
+        filterComment(props.video)
+        
+    }, [props])
+
+    
 
     return(
        
@@ -44,9 +72,10 @@ useEffect (()=>{
                 {
                 comments.map((val, index)=> 
                 <tr key={index} className= "commentDisplay"> 
-                <th>{val.comment} </th>
-                <th><form type="button" onClick={()=> increment(val)} >Like {val.likes}</form></th>
-
+                <th>{val.comment } </th>
+                <th><form type="button" onClick={()=> increment(val)} > Like {val.likes}</form></th>
+                <th><form type = 'submit' onSubmit = {e => handleSubmit(e,val.id)} return false><input name = "reply" onChange={handleChange} placeholder="Reply" type='text'></input></form></th>
+                <tr><DisplayReplies val = {val.id} video = {props.video}/></tr>
                 </tr>)
                 }   
             </div>
